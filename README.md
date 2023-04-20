3831dd044938419992926328f352fe045b12a75c
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-04-20 12:49:00 +0530

    Middleware's Nextjs-APM changes

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# agent-apm-nextjs
-Agent APM for NextJs
+# Getting Started
+
+### agent agent-apm-nextjs
+Description: Agent APM for Next.js
+
+### Prerequisites
+* To monitor APM data on dashboard, [Middleware Host-agent](https://docs.middleware.io/docs/getting-started) needs to be installed, You can refer [this demo project](https://github.com/middleware-labs/demo-apm/tree/master/agent-apm-nextjs) to refer use cases of APM.
+
+### Guides
+To use this APM agent, follow below steps:
+1. Run `npm install @middleware.io/agent-apm-nextjs`.
+2. This feature is experimental, you need to explicitly opt-in by providing below thing into your **next.config.js** file.
+```
+const nextConfig = {
+     ---
+     ---
+     experimental: {
+         instrumentationHook: true
+     }
+     ---
+     ---
+}
+module.exports = nextConfig
+```
+3. Now create a custom `instrumentation.ts` file in your project root directory, and add below code snippet:
+```
+// @ts-ignore
+import { track } from '@middleware.io/agent-apm-nextjs';
+
+export function register() {
+    track({
+        projectName: "<PROJECT-NAME>",
+        serviceName: "<SERVICE-NAME>",
+    });
+}
+```
+That's it. 
---
+++ index.mjs
@@ -0,0 +1,53 @@
+'use strict';
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { NodeTracerProvider, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import process from 'process';
+
+export const track = (args = {}) => {
+
+    const config = {
+        hostUrl: 'http://localhost:9320',
+        projectName: "Project-" + process.pid,
+        serviceName: "Service-" + process.pid,
+        accountKey: "",
+        target: "",
+    }
+
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+        Object.keys(args).forEach(function (key) {
+            if (config.hasOwnProperty(key)) {
+                config[key] = args[key];
+            }
+        });
+
+        let isHostExist = !!(process.env.MW_AGENT_SERVICE && process.env.MW_AGENT_SERVICE !== "");
+        if (isHostExist) {
+            config.hostUrl = `http://${process.env.MW_AGENT_SERVICE}:9320`;
+        }
+
+        const resourceAttributes = {
+            [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
+            ['mw_agent']: true,
+            ['project.name']: config.projectName,
+        };
+
+        if (config.target !== "") {
+            config.hostUrl = config.target;
+            resourceAttributes['mw.account_key'] = config.accountKey;
+        }
+
+        const provider = new NodeTracerProvider({
+            resource: new Resource(resourceAttributes),
+        });
+
+        provider.register();
+
+        provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({
+            url: `${config.hostUrl}/v1/traces`,
+        })));
+
+        return provider;
+    }
+};
