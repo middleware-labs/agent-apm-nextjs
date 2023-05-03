3782fdfc97a0ba3fa4cbffeeb720013bab73e276
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-05-03 15:43:08 +0530

    Middleware's Nextjs-APM changes

--- index.mjs
+++ index.mjs
@@ -7,49 +7,44 @@ import process from 'process';
 
 export const track = (args = {}) => {
 
+    if (process.env.NEXT_RUNTIME !== 'nodejs') {
+        return;
+    }
+
     const config = {
         hostUrl: 'http://localhost:9320',
         projectName: "Project-" + process.pid,
         serviceName: "Service-" + process.pid,
         accountKey: "",
         target: "",
+        ...args,
+    };
+
+    const resourceAttributes = {
+        [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
+        ['mw_agent']: true,
+        ['project.name']: config.projectName,
+        ...(config.accountKey !== "" && {'mw.account_key': config.accountKey}),
+    };
+
+    if (config.target !== "") {
+        config.hostUrl = config.target;
     }
 
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-        Object.keys(args).forEach(function (key) {
-            if (config.hasOwnProperty(key)) {
-                config[key] = args[key];
-            }
-        });
-
-        const resourceAttributes = {
-            [SemanticResourceAttributes.SERVICE_NAME]: config.serviceName,
-            ['mw_agent']: true,
-            ['project.name']: config.projectName,
-        };
-
-        if (config.accountKey !== "") {
-            resourceAttributes['mw.account_key'] = config.accountKey;
-        }
-
-        if (config.target !== "") {
-            config.hostUrl = config.target;
-        }
-
-        if (!!(process.env.MW_AGENT_SERVICE && process.env.MW_AGENT_SERVICE !== "")) {
-            config.hostUrl = `http://${process.env.MW_AGENT_SERVICE}:9320`;
-        }
-
-        const provider = new NodeTracerProvider({
-            resource: new Resource(resourceAttributes),
-        });
-
-        provider.register();
-
-        provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({
-            url: `${config.hostUrl}/v1/traces`,
-        })));
-
-        return provider;
+    if (!!(process.env.MW_AGENT_SERVICE && process.env.MW_AGENT_SERVICE !== "")) {
+        config.hostUrl = `http://${process.env.MW_AGENT_SERVICE}:9320`;
     }
+
+    const configUrl = ((config.target).toLowerCase() === "vercel") ? {} : {url: `${config.hostUrl}/v1/traces`};
+
+    const provider = new NodeTracerProvider({
+        resource: new Resource(resourceAttributes),
+    });
+
+    provider.register();
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter(configUrl)));
+
+    return provider;
+
 };
