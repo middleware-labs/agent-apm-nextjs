2921d6b4efb91ce1c059edb36082652e6997c59e
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-06-09 17:50:50 +0530

    Logs enabled for Nextjs

--- index.edge.js
+++ index.edge.js
@@ -1,7 +1,31 @@
 'use strict';
 
 export const track = (args = {}) => {
-
     return true;
-
 };
+
+const info = (message, attributes = {}) => {
+    return true;
+};
+
+const warn = (message, attributes = {}) => {
+    return true;
+};
+
+const debug = (message, attributes = {}) => {
+    return true;
+};
+
+const error = (message, attributes = {}) => {
+    return true;
+};
+
+const tracker = {
+    track,
+    info,
+    error,
+    warn,
+    debug,
+};
+
+export default tracker;
